c403dcb925a9aa5e90f07f9db1d03ef1a179037b
Ronique Reid <roni.reid.roni@gmail.com>
2022-04-04 21:54:23 -0500

    changes made

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,22 @@
 <script>
+import NewsList from '@/components/NewsList.vue'
 export default {
     data() {
         return {
-            message: "Hello World! Welcome to VueJS"
+            message: "Hello World! Welcome to INFO3180 Lab6"
         };
     },
+    components: { NewsList }
 }
 </script>
 
 <template>
     <div class="container">
-      <div class="text-center">
-        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-        <h1>{{ message }}</h1>
-      </div>
+        <div class="text-center">
+            <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+            <h1>{{ message }}</h1>
+            <NewsList />
+        </div>
     </div>
 </template>
 
